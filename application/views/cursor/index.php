25e2904252035d0d107eeaa17c1d8d49153bdd46
cu francis <cufrancis.com@gmail.com>
2017-04-03 20:22:41 +0800

    更新播放器插件

--- application/views/cursor/index.php
+++ application/views/cursor/index.php
@@ -1,3 +1,5 @@
+<link href="//cdn.bootcss.com/plyr/2.0.12/plyr.css" rel="stylesheet">
+
 <div class="container">
     <h2><?php echo $cursor->name;?></h2>
     <p>
@@ -11,38 +13,47 @@
     <h2>视频列表</h2>
     <div id="video_list">
         <?php foreach($videos as $video):?>
-            <a class="video_click" id=<?=$video->id;?> url="<?=$video->url;?>"><?php echo $video->name;?></a><br />
+            <a class="video_click" id=<?=$video->id;?> vid="<?php echo $video->id;?>" src="<?=$video->url;?>"><?php echo $video->name;?></a><br />
         <?php endforeach;?>
-        <div id="video_index"></div> <!-- 播放区域 -->
+
+        <div id="video_index">
+            <video id="video" poster="https://cdn.selz.com/plyr/1.5/View_From_A_Blue_Moon_Trailer-HD.jpg?v1" controls>
+              <source src="https://cdn.selz.com/plyr/1.5/View_From_A_Blue_Moon_Trailer-HD.mp4" type="video/mp4">
+              <!-- <source src="https://cdn.selz.com/plyr/1.5/View_From_A_Blue_Moon_Trailer-HD.webm" type="video/webm"> -->
+              <!-- Captions are optional -->
+              <track kind="captions" label="English captions" src="https://cdn.selz.com/plyr/1.5/View_From_A_Blue_Moon_Trailer-HD.en.vtt" srclang="en" default>
+            </video>
+        </div> <!-- 播放区域 -->
     </div>
 
     <h2>试题</h2>
+    <div class="st"></div>
     <h2>评论区</h2>
 
 </div>
 
 <script src="//cdn.bootcss.com/jquery/3.2.1/jquery.js"></script>
 <script src="//cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.js"></script>
-<script type="text/javascript">
+<script src="//cdn.bootcss.com/plyr/2.0.12/plyr.js"></script>
+<script src="//cdn.bootcss.com/plyr/2.0.12/plyr.svg"></script>
+<script>
+
 $(document).ready(function(){
+    plyr.setup();
+
     $('.video_click').click(function(){
-        // 先删除子元素
-        // 再添加video
-
-        var video_div  = $('#video_index');
-        var video_cick = $('.video_cick');
-        var base_url = "<?php echo base_url();?>"
-        var url = base_url+$(this).attr('url');
-
-        video_div.empty();
-        video_div.append('<video src="'+url+'" controls="controls">您的浏览器不支持 video 标签。</video>')
-        video_div.fadeToggle();
-
-        // alert(this.id)
+        // 单击事件
+        var url = "<?=base_url();?>"+$(this).attr('src')
+        var video = $('#video');
+        video.attr('src', url);
+        alert(video.attr('src'));
     });
-});
+    // 更新 video地址
+    // var video = $('#video');
+    // alert(video)
+    // video.attr('src', 'test')
+    // // source = 'src';
+    // alert($('#video').attr('src'));
 
-// 把视频播放时间调整到上次退出时间
-// myVid=document.getElementById("video");
-// myVid.currentTime=5;
+})
 </script>
